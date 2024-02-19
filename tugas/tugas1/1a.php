c121a1624866c20c58ece7653e42fbc993ce8512
Ichaaprilia <ap8771067@gmail.com>
2024-02-19 23:36:31 +0700

    mengerjakan tugas1

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,7 @@
+<?php 
+$topi = "Topi";
+$bundar = "Bundar";
+
+echo "<i><h2>" . '"' . "$topi saya $bundar, $bundar $topi saya." . '"' . "</h2></i>"
+
+?>
---
+++ tugas/tugas1/1c.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+    <style>
+        .kotak {
+            width: 75px;
+            height: 75px;
+            background-color: yellow;
+            border: 3px solid black;
+            color: black;
+            display: flex;
+        }
+        .kotak h3 {
+            margin: auto;
+        }
+        .row {
+            display: flex;
+        }
+        </style>
+</head>
+<body>
+
+    <div class="row row-1">
+        <div class="kotak kotak1">
+            <h3>1</h3>
+        </div>
+    </div>
+
+    <div class="row row-2">
+        <div class="kotak kotak2"><h3>2</h3></div>
+        <div class="kotak kotak2"><h3>2</h3></div>
+    </div>
+
+    <div class="row row-3">
+        <div class="kotak kotak3"><h3>3</h3></div>
+        <div class="kotak kotak3"><h3>3</h3></div>
+        <div class="kotak kotak3"><h3>3</h3></div>
+    </div>
+</body>
+</html>
